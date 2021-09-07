f644425f46a4b59780784b1898d168e47c6e6039
Kent Brockman <brockman.ks@gmail.com>
2021-09-07 12:15:29 -0600

    Initial commit before I break more stuff lol

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const drug_data = require('./data.json');
+
+var app = express();
+
+nunjucks.configure('', {
+    autoescape: true,
+    express: app
+});
+
+console.log(drug_data);
+app.get('/', function(req, res) {
+    // res.send(nunjucks.renderString('Hello {{ username }}', { username: 'James' }));
+    // res.send(nunjucks.render('test_template.html', { username: 'James' }));
+    res.send(nunjucks.render('monographs.html', drug_data));
+});
+
+port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
